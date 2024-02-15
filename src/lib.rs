dcd2d98acd381dc354d442d54f21e4d833236975
Fabian <spiele2014fs@gmail.com>
2024-02-15 15:55:14 +0100

    Make client-gen a feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+#[cfg(feature = "client-gen")]
 mod gen;
 mod method;
 mod types;
 
 pub use std::error::Error as StdError;
 
+#[cfg(feature = "client-gen")]
 pub use gen::generate;
 pub use types::{generator, Kind, RequestInfo, Requests, Tag};
